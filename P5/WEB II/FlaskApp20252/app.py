5b49eb25d06e3beb90bfc9c636417455c2f21787
AquilaDavid <aquiladavid07@gmail.com>
2025-10-09 16:12:12 -0300

    Update no arquivo app.py e Instituição de Ensino

--- P5/WEB II/FlaskApp20252/app.py
+++ P5/WEB II/FlaskApp20252/app.py
@@ -11,20 +11,6 @@ usuarios = carregar_usuarios()
 def index():
     return '{"versao":"2.0.0"}', 200
 
-'''@app.get("/usuarios")
-def get_usuarios():
-    return jsonify(usuarios)
-
-@app.get("/usuarios/<int:id>")
-def get_usuario_por_id(id: int):
-    return jsonify(usuarios[id])
-
-@app.post("/usuarios")
-def adicionar_usuario():
-    data = request.get_json()
-    usuario = {"nome": data['nome']}
-    usuarios.append(usuario)
-    return usuario, 201'''
 
 @app.get("/instituicoesensino")
 def get_instituicoes_ensino():
--- P5/WEB II/FlaskApp20252/models/InstituicaoEnsino.py
+++ P5/WEB II/FlaskApp20252/models/InstituicaoEnsino.py
@@ -1,15 +1,20 @@
 import json
 import os
 
-class InstituicaoEnsino:
+class InstituicaoEnsino():
 
-    def __init__(self, codigo_instituicao, nome, codigo_estado, codigo_municipio,
+    def __init__(self, codigo_instituicao, nome_instituicao, codigo_estado, nome_estado, codigo_municipio, nome_municipio, codigo_regiao, nome_regiao,
                  matriculas_educacao_basica, matriculas_profissionalizante,
                  matriculas_eja, matriculas_especiais):
+        
         self.codigo_instituicao = codigo_instituicao
-        self.nome = nome
+        self.nome_instituicao = nome_instituicao
         self.codigo_estado = codigo_estado
+        self.nome_estado = nome_estado
         self.codigo_municipio = codigo_municipio
+        self.nome_municipio = nome_municipio
+        self.codigo_regiao = codigo_regiao
+        self.nome_regiao = nome_regiao
         self.matriculas_educacao_basica = matriculas_educacao_basica
         self.matriculas_profissionalizante = matriculas_profissionalizante
         self.matriculas_eja = matriculas_eja
@@ -21,9 +26,13 @@ class InstituicaoEnsino:
     def to_json(self):
         return {
             "codigo_instituicao": self.codigo_instituicao,
-            "nome": self.nome,
+            "nome_instituicao": self.nome_instituicao,
             "codigo_estado": self.codigo_estado,
+            "nome_estado": self.nome_estado,
             "codigo_municipio": self.codigo_municipio,
+            "nome_municipio": self.nome_municipio,
+            "codigo_regiao": self.codigo_regiao,
+            "nome_regiao": self.nome_regiao,
             "matriculas_educacao_basica": self.matriculas_educacao_basica,
             "matriculas_profissionalizante": self.matriculas_profissionalizante,
             "matriculas_eja": self.matriculas_eja,
@@ -40,7 +49,25 @@ def carregar_dados():
     if os.path.exists(arquivo_json):
         with open(arquivo_json, "r", encoding="utf-8") as arquivo:
             dados = json.load(arquivo)
-            return [InstituicaoEnsino(**dado) for dado in dados]
+            lista = []
+            for d in dados:
+                inst = InstituicaoEnsino(
+                    codigo_instituicao=d.get("codigo", ""),
+                    nome_instituicao=d.get("nome", ""),
+                    codigo_estado=d.get("estado", {}).get("codigo", ""),
+                    nome_estado=d.get("estado", {}).get("nome", ""),
+                    codigo_municipio=d.get("municipio", {}).get("codigo", ""),
+                    nome_municipio=d.get("municipio", {}).get("nome", ""),
+                    codigo_regiao=d.get("regiao", {}).get("codigo", ""),
+                    nome_regiao=d.get("regiao", {}).get("nome", ""),
+                    matriculas_educacao_basica=d.get("qt_mat_bas", 0),
+                    matriculas_profissionalizante=d.get("qt_mat_prof", 0),
+                    matriculas_eja=d.get("qt_mat_eja", 0),
+                    matriculas_especiais=d.get("qt_mat_esp", 0)
+                )
+                lista.append(inst)
+            return lista
+
     return []
 
 lista_instituicoes = carregar_dados()
